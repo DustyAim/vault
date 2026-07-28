d4fe6a045f46794b46d94d4c3afec9ed3891fecf
dusty <dmitriysholota@gmail.com>
2026-07-28 21:41:42 +0300

    [8kyu] {C++} Sum of elements**2 in vector

---
+++ 8-kyu/Square_size_of_vector_els.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+
+int square_sum(const std::vector<int>& numbers)
+{
+    int result = 0;
+    for (int i = 0; i < numbers.size() ; i++)
+        result += numbers[i]*numbers[i];
+    return result;
+}
