826db8f201da2d005d899fc6d9cf8a50504144ba
dusty <dmitriysholota@gmail.com>
2026-07-29 00:36:46 +0300

    [8kyu] {C++} progressive citizens population during years

---
+++ 8-kyu/progressive_population.cpp
@@ -0,0 +1,17 @@
+class Arge
+{
+public:
+    static int nbYear(int p0, double percent, int aug, int p)
+    {
+        int p_cur = p0;
+        int years = 0;
+
+        while (p_cur <= p)
+        {
+            p_cur = p0 * (1 + percent / 100) + aug;
+            p0 = p_cur;
+            years++;
+        }
+        return years;
+    }
+};
