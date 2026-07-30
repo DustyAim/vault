0e790fae19798ca94d4a4eab4d274c927fa4f402
dusty <dmitriysholota@gmail.com>
2026-07-31 00:59:19 +0300

    6kyu Interesting multiplication algoritm

--- 6-kyu/persistant_bugger.cpp
+++ 6-kyu/persistant_bugger.cpp
@@ -1,3 +1,28 @@
-//
-// Created by Sholota Dmitriy on 31.07.2026.
-//
+
+
+#include <vector>
+
+int persistence(long long n) {
+    std::vector<int> digits;
+    int counter = 0;
+    
+    if (n < 10) return 0; 
+
+    while (n >= 10) {
+        digits.clear();             
+
+        long long temp = n;
+        while (temp > 0) {          
+            digits.push_back(temp % 10);
+            temp /= 10;
+        }
+
+        long long product = 1;
+        for (int d : digits) {
+            product *= d;
+        }
+        n = product;
+        counter++;                  
+    }
+    return counter;
+}
