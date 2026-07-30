94c75c61a45cd57d3a8cfacb568fa419cc0b19d8
dusty <dmitriysholota@gmail.com>
2026-07-31 00:56:31 +0300

    6kyu Brilliant realization for deleting too popular elements in arr

--- 6-kyu/delete_excess_photos_in_unorderered_map.cpp
+++ 6-kyu/delete_excess_photos_in_unorderered_map.cpp
@@ -1,3 +1,13 @@
-//
-// Created by Sholota Dmitriy on 30.07.2026.
-//
+std::vector<int> deleteNth(std::vector<int> arr, int n)
+{
+    std::unordered_map<int, int> times_of_photos;
+    std::vector<int> result;
+
+    for (int x : arr){
+        if (times_of_photos[x] < n){
+            result.push_back(x);
+            times_of_photos[x]++;
+        }
+    }
+    return result;
+}
