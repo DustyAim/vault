b35b56a7c2e0355f1ecf7bb6abc019b1335a79b3
dusty <dmitriysholota@gmail.com>
2026-07-31 00:55:35 +0300

    7 kyu work with pair vector summarizing .first .second

--- 7-kyu/pair_vector_busStops.cpp
+++ 7-kyu/pair_vector_busStops.cpp
@@ -1,3 +1,9 @@
-//
-// Created by Sholota Dmitriy on 30.07.2026.
-//
+#include <utility>
+#include <vector>
+
+unsigned int number(const std::vector<std::pair<int, int>>& busStops){
+    int result = 0;
+    for (int i = 0; i< busStops.size(); i++)
+        result += busStops[i].first - busStops[i].second;
+    return result;
+}
