5aaf45efa0275c1c4269b107d1f1324435a8a26a
dusty <dmitriysholota@gmail.com>
2026-07-31 00:57:36 +0300

    6kyu Print the word with highest score in string

--- 6-kyu/highest_scoring_word.cpp
+++ 6-kyu/highest_scoring_word.cpp
@@ -1,3 +1,25 @@
-//
-// Created by Sholota Dmitriy on 30.07.2026.
-//
+#include <string>
+
+std::string highestScoringWord(const std::string &str) {
+    int highest_score = 0;
+    int sum = 0;
+    std::string current_word;
+    std::string best_word;
+    for (char c : str) {
+        if (c != ' ') {
+            sum += (c - 'a'+1); 
+            current_word += c;
+        } else {
+            if (sum > highest_score) {
+                highest_score = sum;
+                best_word = current_word;
+            }
+            sum = 0;
+            current_word.clear();
+        }
+    }
+    if (sum > highest_score) {
+        best_word = current_word;
+    }
+    return best_word;
+}
