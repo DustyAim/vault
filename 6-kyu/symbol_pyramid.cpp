3d4f91dad91a6793e6b7be68bfda89b4065e6c82
dusty <dmitriysholota@gmail.com>
2026-07-31 01:00:06 +0300

    6kyu The world shortest realization of drowing vector<string> pyromid

--- 6-kyu/symbol_pyramid.cpp
+++ 6-kyu/symbol_pyramid.cpp
@@ -1,3 +1,9 @@
-//
-// Created by Sholota Dmitriy on 31.07.2026.
-//
+#include <string>
+#include <vector>
+
+std::vector<std::string> towerBuilder(unsigned nFloors) {
+    std::vector<std::string> tower;
+    for (int i = 0; i < nFloors; i++)
+        tower.push_back(std::string(nFloors-1-i, ' ') + std::string(2*i+1, '*') +std::string(nFloors-1-i, ' '));
+    return tower;
+}
