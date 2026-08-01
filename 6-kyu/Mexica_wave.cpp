a731837c71dc0e21194657fb7f79c866b862e382
dusty <dmitriysholota@gmail.com>
2026-08-02 01:43:56 +0300

    mexica wave of str word

--- 6-kyu/Mexica_wave.cpp
+++ 6-kyu/Mexica_wave.cpp
@@ -1,3 +1,17 @@
-//
-// Created by Sholota Dmitriy on 01.08.2026.
-//
+std::vector<std::string> wave(std::string y){
+    int upper_position = 0;
+    std::string y1 = y;
+    std::vector<std::string> wave;
+    while (upper_position != y.size()){
+        if (y1[upper_position]== ' ') upper_position++;
+        else
+        {
+            y1[upper_position] -= 32;
+            wave.push_back(y1);
+            upper_position++;
+            y1 = y;
+        }
+    }
+
+    return wave;
+}
