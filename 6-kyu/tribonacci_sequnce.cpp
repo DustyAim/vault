bdb1729a67232a4ebdf953ab80ac0390778d8097
dusty <dmitriysholota@gmail.com>
2026-08-01 03:19:54 +0300

    6kyu Tribonacci numbers

--- 6-kyu/tribonacci_sequnce.cpp
+++ 6-kyu/tribonacci_sequnce.cpp
@@ -1,3 +1,14 @@
-//
-// Created by Sholota Dmitriy on 01.08.2026.
-//
+std::vector<int> tribonacci(std::vector<int> signature, int n)
+{
+    std::vector<int> result = signature;
+    if (n<3){
+        signature.resize(n);
+        return signature;
+    }
+    int i = signature.size();
+    while (result.size() < n){
+        result.push_back(result[result.size()-3] + result[result.size()-2]+ result[result.size()-1]);
+    }
+    return result;
+
+}
