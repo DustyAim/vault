1241aff4296228d22ec815465584ce04659fff5c
dusty <dmitriysholota@gmail.com>
2026-08-02 01:44:48 +0300

    interesting relization of maths check for ints and their squares

--- 6-kyu/are_same_vectors_int_and_squares.cpp
+++ 6-kyu/are_same_vectors_int_and_squares.cpp
@@ -1,3 +1,20 @@
-//
-// Created by Sholota Dmitriy on 01.08.2026.
-//
+#include <vector>
+#include <algorithm>
+
+class Same {
+public:
+    static bool comp(std::vector<int>& a, std::vector<int>& b) {
+        if (a.size() != b.size()) return false;
+
+        std::vector<int> squares;
+        for (int x : a) squares.push_back(x * x);
+
+        std::sort(squares.begin(), squares.end());
+        std::sort(b.begin(), b.end());
+
+        for (size_t i = 0; i < squares.size(); ++i) {
+            if (squares[i] != b[i]) return false;
+        }
+        return true;
+    }
+};
