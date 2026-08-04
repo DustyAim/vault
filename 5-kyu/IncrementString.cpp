3492f06631d557c7a0ab01153366f685ad05aa60
dusty <dmitriysholota@gmail.com>
2026-08-05 02:10:06 +0300

    Hard shit of increasing number sufix in strings

--- 5-kyu/IncrementString.cpp
+++ 5-kyu/IncrementString.cpp
@@ -1,3 +1,21 @@
-//
-// Created by Sholota Dmitriy on 05.08.2026.
-//
+std::string incrementString(const std::string &str) {
+
+    size_t pos = str.length();
+    while (pos > 0 && isdigit(str[pos - 1])) {
+        --pos;
+    }
+    std::string prefix = str.substr(0, pos);
+    std::string suffix = str.substr(pos);
+
+    if (suffix.empty())
+        return str + "1";
+
+    size_t len = suffix.length();
+    unsigned long long num = std::stoull(suffix) + 1;
+    std::string newSuffix = std::to_string(num);
+
+    if (newSuffix.length() < len)
+        newSuffix = std::string(len - newSuffix.length(), '0') + newSuffix;
+
+    return prefix + newSuffix;
+}
