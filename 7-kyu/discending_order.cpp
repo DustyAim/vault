33c7452f90326cd7827ad160d645a42e47233877
dusty <dmitriysholota@gmail.com>
2026-08-04 22:49:22 +0300

    Discending order of uint

--- 7-kyu/discending_order.cpp
+++ 7-kyu/discending_order.cpp
@@ -1,3 +1,17 @@
-//
-// Created by Sholota Dmitriy on 01.08.2026.
-//
+#include <cinttypes>
+
+uint64_t descendingOrder(uint64_t a)
+{
+    std::vector<int> digits;
+    if (a==0) return 0;
+    while (a>0){
+        digits.push_back(a%10);
+        a /= 10;
+    }
+    std::sort(digits.begin(), digits.end(), std::greater<int>());
+    uint64_t number = 0;
+    for (int d : digits) {
+        number = number * 10 + d;
+    }
+    return number;
+}
