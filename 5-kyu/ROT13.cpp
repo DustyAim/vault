f6bf9d1f925af1917addd5c0a3ed94ecfb667a75
dusty <dmitriysholota@gmail.com>
2026-08-05 02:09:19 +0300

    Clever realisation of Ceaser's code

--- 5-kyu/ROT13.cpp
+++ 5-kyu/ROT13.cpp
@@ -1,3 +1,15 @@
-//
-// Created by Sholota Dmitriy on 05.08.2026.
-//
+#include <string>
+using namespace std;
+
+string rot13(string msg)
+{
+    std::string result;
+    for (char c : msg){
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+            int base = (c >= 'a') ? 'a' : 'A';   // определяем базу
+            result += base + (c - base + 13) % 26; }
+        else
+            result += c;
+    }
+    return result;
+}
