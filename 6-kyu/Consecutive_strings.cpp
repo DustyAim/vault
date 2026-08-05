3999687b49030a529c7b130dc36439bfd11a217f
dusty <dmitriysholota@gmail.com>
2026-08-05 23:25:05 +0300

    playing with strings

--- 6-kyu/Consecutive_strings.cpp
+++ 6-kyu/Consecutive_strings.cpp
@@ -1,3 +1,24 @@
-//
-// Created by Sholota Dmitriy on 05.08.2026.
-//
+#include <string>
+#include <vector>
+
+class LongestConsec {
+public:
+    static std::string longestConsec(const std::vector<std::string>& strarr, int k) {
+        if (strarr.size() == 0 || k <= 0 || k > strarr.size()) return "";
+      
+        std::vector<std::string> combs(strarr.size() - k + 1);
+        for (int i = 0; i <= strarr.size() - k; ++i)
+            for (int j = i; j < i + k; ++j)
+                combs[i] += strarr[j];
+
+        int bestIdx = 0;
+        int maxLen = combs[0].size();
+        for (int i = 1; i < combs.size(); ++i) {
+            if (combs[i].size() > maxLen) {
+                maxLen = combs[i].size();
+                bestIdx = i;
+            }
+        }
+        return combs[bestIdx];
+    }
+};
