4811cf21d8d2e4678ff188145adea31211098350
dusty <dmitriysholota@gmail.com>
2026-08-05 23:28:00 +0300

    Is number a product of fibonaccies?

--- 5-kyu/Product_of_consecutive_Fib_numbers.c
+++ 5-kyu/Product_of_consecutive_Fib_numbers.c
@@ -1,3 +1,26 @@
-//
-// Created by Sholota Dmitriy on 05.08.2026.
-//
+#include <vector>
+typedef unsigned long long ull;
+class ProdFib
+{
+public:
+    static std::vector<ull> productFib(ull prod){
+        std::vector<unsigned long long> fibonacci;
+        fibonacci.push_back(0);
+        fibonacci.push_back(1);
+
+        unsigned long long sum = 0;
+        int n = 0;
+        while (true){
+            sum = fibonacci[n] + fibonacci[n+1];
+
+            if (fibonacci[n] * fibonacci[n+1] == prod)
+                return {fibonacci[n], fibonacci[n+1], 1ULL};
+
+            if (fibonacci[n] * fibonacci[n+1] > prod)
+                return {fibonacci[n], fibonacci[n+1], 0ULL};
+
+            fibonacci.push_back(sum);
+            n++;
+        }
+    }
+};
