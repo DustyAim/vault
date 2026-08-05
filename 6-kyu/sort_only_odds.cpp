9ab129e55731c49c788a1566ec121ef0ed4e66ea
dusty <dmitriysholota@gmail.com>
2026-08-05 23:27:13 +0300

    Sort only odds in int vector

--- 6-kyu/sort_only_odds.cpp
+++ 6-kyu/sort_only_odds.cpp
@@ -1,3 +1,21 @@
-//
-// Created by Sholota Dmitriy on 05.08.2026.
-//
+class Kata
+{
+public:
+    std::vector<int> sortArray(std::vector<int> array)
+    {
+        std::vector<int> odds;
+        for (int i = 0; i < array.size(); i++)
+            if (array[i]%2!=0) odds.push_back(array[i]);
+
+        std::sort(odds.begin(), odds.end());
+
+        int pos = 0;
+        for (int j = 0; j < array.size(); j++){
+            if (array[j]%2!=0){
+                array[j] = odds[pos];
+                pos++;
+            }
+        }
+        return array;
+    }
+};
