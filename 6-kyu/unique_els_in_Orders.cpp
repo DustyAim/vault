245521d930544adcf3cf2af455271cf4c3397e34
dusty <dmitriysholota@gmail.com>
2026-08-05 23:26:33 +0300

    Short str to new str of unique chars

--- 6-kyu/unique_els_in_Orders.cpp
+++ 6-kyu/unique_els_in_Orders.cpp
@@ -1,3 +1,25 @@
-//
-// Created by Sholota Dmitriy on 05.08.2026.
-//
+#include <string>
+#include <vector>
+
+template <typename T> std::vector<T> uniqueInOrder(const std::vector<T>& iterable){
+    std::vector<T> result;
+    if (iterable.empty()) return result;
+
+    result.push_back(iterable[0]);
+    for (size_t i = 1; i < iterable.size(); ++i)
+        if (iterable[i] != iterable[i - 1]) result.push_back(iterable[i]);
+
+    return result;
+}
+
+std::vector<char> uniqueInOrder(const std::string& iterable){
+    std::vector<char> result;
+    if (iterable.empty()) return result;
+
+    result.push_back(iterable[0]);
+    for (int i = 1; i < iterable.size(); i++)
+        if (iterable[i] != iterable[i-1]) result.push_back(iterable[i]);
+
+    return result;
+
+}
