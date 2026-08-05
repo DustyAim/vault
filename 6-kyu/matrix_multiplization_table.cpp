12cea4055a327079f94a66177ab939957e335a4e
dusty <dmitriysholota@gmail.com>
2026-08-05 23:25:45 +0300

    Drawing vector vector multiplication matrix

--- 6-kyu/matrix_multiplization_table.cpp
+++ 6-kyu/matrix_multiplization_table.cpp
@@ -1,3 +1,10 @@
-//
-// Created by Sholota Dmitriy on 05.08.2026.
-//
+   #include <vector>
+
+using namespace std;
+vector<vector<int>> multiplication_table(int n){
+    std::vector<std::vector<int>> matrix(n, vector<int>(n, 0));
+    for (int col = 0; col < n; col++)
+        for (int row = 0; row < n; row++)
+            matrix[col][row] = (col+1)*(row+1);
+    return matrix;
+}
